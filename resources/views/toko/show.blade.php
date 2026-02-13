2df33e8093ae7bb6b78108df928e7697df797cc4
fajarfaturrahman2120 <fajarfaturrahman2120@gmail.com>
2026-02-13 11:51:28 +0700

    fix

--- resources/views/toko/show.blade.php
+++ resources/views/toko/show.blade.php
@@ -7,13 +7,37 @@
     <h4 class="mb-3">Detail Toko</h4>
 
     <div class="card p-4 shadow-sm">
-        <p><strong>Nama Toko:</strong> {{ $toko->name }}</p>
-        <p><strong>No HP:</strong> {{ $toko->no_hp }}</p>
-        <p><strong>Alamat:</strong> {{ $toko->alamat }}</p>
-        <p><strong>Username:</strong> {{ $toko->username }}</p>
+        <div class="row">
+            <!-- KIRI -->
+            <div class="col-md-6">
+                <div class="mb-3">
+                    <label>Nama Toko</label>
+                    <input type="text" class="form-control bg-light" value="{{ $toko->name }}" readonly>
+                </div>
 
-        <a href="{{ route('toko.edit', $toko->id_toko) }}" class="btn btn-warning">Edit</a>
-        <a href="{{ route('toko.index') }}" class="btn btn-secondary">Kembali</a>
+                <div class="mb-3">
+                    <label>No HP</label>
+                    <input type="text" class="form-control bg-light" value="{{ $toko->no_hp }}" readonly>
+                </div>
+            </div>
+
+            <!-- KANAN -->
+            <div class="col-md-6">
+                <div class="mb-3">
+                    <label>Alamat</label>
+                    <input type="text" class="form-control bg-light" value="{{ $toko->alamat }}" readonly>
+                </div>
+
+                <div class="mb-3">
+                    <label>Username</label>
+                    <input type="text" class="form-control bg-light" value="{{ $toko->username }}" readonly>
+                </div>
+            </div>
+        </div>
+
+        <div class="text-end mt-3">
+            <a href="{{ route('toko.index') }}" class="btn btn-secondary">Kembali</a>
+        </div>
     </div>
 </div>
 @endsection
